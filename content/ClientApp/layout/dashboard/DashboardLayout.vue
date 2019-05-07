2ecfa2b90f2362ed35ef6b83bca12b068f3fce26
Brian Shimek <brian.shimek@gmail.com>
2019-05-07 15:59:15 -0500

    Dashboard template completed

--- content/ClientApp/layout/dashboard/DashboardLayout.vue
+++ content/ClientApp/layout/dashboard/DashboardLayout.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="wrapper">
-    <side-bar>
+    <side-bar :background-color="backgroundColor">
       <template slot="links">
-        <sidebar-link to="/dashboard" :name="$t('sidebar.dashboard')" icon="tim-icons icon-chart-pie-36"/>
-        <sidebar-link to="/icons" :name="$t('sidebar.icons')" icon="tim-icons icon-atom"/>
-        <sidebar-link to="/maps" :name="$t('sidebar.maps')" icon="tim-icons icon-pin"/>
-        <sidebar-link to="/notifications" :name="$t('sidebar.notifications')" icon="tim-icons icon-bell-55"/>
-        <sidebar-link to="/profile" :name="$t('sidebar.userProfile')" icon="tim-icons icon-single-02"/>
-        <sidebar-link to="/table-list" :name="$t('sidebar.tableList')" icon="tim-icons icon-puzzle-10"/>
-        <sidebar-link to="/typography" :name="$t('sidebar.typography')" icon="tim-icons icon-align-center"/>
-        <sidebar-link to="/dashboard?enableRTL=true" :name="$t('sidebar.rtlSupport')" icon="tim-icons icon-world"/>
+        <sidebar-link to="/dashboard" :name="$t('sidebar.dashboard')" icon="tim-icons icon-chart-pie-36" />
+        <sidebar-link to="/icons" :name="$t('sidebar.icons')" icon="tim-icons icon-atom" />
+        <sidebar-link to="/maps" :name="$t('sidebar.maps')" icon="tim-icons icon-pin" />
+        <sidebar-link to="/notifications" :name="$t('sidebar.notifications')" icon="tim-icons icon-bell-55" />
+        <sidebar-link to="/profile" :name="$t('sidebar.userProfile')" icon="tim-icons icon-single-02" />
+        <sidebar-link to="/table-list" :name="$t('sidebar.tableList')" icon="tim-icons icon-puzzle-10" />
+        <sidebar-link to="/typography" :name="$t('sidebar.typography')" icon="tim-icons icon-align-center" />
+        <sidebar-link to="/dashboard?enableRTL=true" :name="$t('sidebar.rtlSupport')" icon="tim-icons icon-world" />
       </template>
     </side-bar>
-    <div class="main-panel">
+    <sidebar-share :background-color.sync="backgroundColor">
+    </sidebar-share>
+    <div class="main-panel" :data="backgroundColor">
       <top-navbar></top-navbar>
 
       <dashboard-content @click.native="toggleSidebar">
@@ -30,13 +32,21 @@ import TopNavbar from "./TopNavbar.vue";
 import ContentFooter from "./ContentFooter.vue";
 import DashboardContent from "./Content.vue";
 import MobileMenu from "./MobileMenu.vue";
+import SidebarShare from "./SidebarSharePlugin.vue";
+
 export default {
   components: {
     TopNavbar,
     ContentFooter,
     DashboardContent,
-    MobileMenu
-  },
+    MobileMenu,
+    SidebarShare
+    },
+    data() {
+      return {
+        backgroundColor: "vue"
+      };
+    },
   methods: {
     toggleSidebar() {
       if (this.$sidebar.showSidebar) {
